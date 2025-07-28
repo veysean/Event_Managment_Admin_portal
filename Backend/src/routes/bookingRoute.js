a6331c23a2933320fbb247a1f2f02806de58d475
veysean <veyseankem@gmail.com>
2025-07-28 12:03:47 +0700

    API for event

--- Backend/src/routes/bookingRoute.js
+++ Backend/src/routes/bookingRoute.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { getAllEvents} from '../controllers/bookingController.js';
-import { authenticateToken } from '../middlewares/auth.middleware.js';
-const EventRouter = express.Router();    
-EventRouter.use(authenticateToken);
+import { 
+    getEvents,
+    deleteEvent,
+    updateEvent,
 
-EventRouter.get('/',getAllEvents);
+} from '../controllers/bookingController.js';
+import { authenticateToken } from '../middlewares/auth.middleware.js';
+import { upload } from "../config/multerConfig.js";
+const EventRouter = express.Router();    
+//EventRouter.use(authenticateToken);
+
+EventRouter.get('/', getEvents);
+EventRouter.delete('/:id', deleteEvent);
+EventRouter.put('/:id', upload.none(), updateEvent);
 
 export default EventRouter;
