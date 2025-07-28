f4a2d4a429079d5539426c064b6aa48923399887
OengGechty <gechtyoeng@gmail.com>
2025-07-29 00:45:13 +0700

    add event to return with total lenght

--- Backend/src/controllers/bookingController.js
+++ Backend/src/controllers/bookingController.js
@@ -193,7 +193,7 @@ export const getEvents = async (req, res) => {
       order
     });
 
-    res.status(200).json(events);
+    res.status(200).json({ total: events.length, events });
   } catch (error) {
     console.error('Error fetching events:', error);
     res.status(500).json({ message: 'Server error while fetching events' });
